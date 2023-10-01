375f1f38970c24cdcfa8a79e6d39848040dd2731
SUPDAWGIE <shofpita@gmail.com>
2023-10-01 14:12:19 +0300

    Init Commit

---
+++ Build.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="D:\Develop\Projects\supproject\SupProject\SupProject.uproject" ^
+-platform=Win64 -clientconfig=Shipping ^
+-archivedirectory="C:\Develop\Projects\supproject\SupProject\BuildFromBat" ^
+-build -cook -stage -stagingdirectory="C:\Develop\Projects\supproject\SupProject\Stage" ^
+-skippackage -archive -pak -package
+
